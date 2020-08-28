6d740b79c399fbefaedc43751c4eac04a86a0f06
flpmanso <43483925+flpmanso@users.noreply.github.com>
2020-08-28 20:29:34 -0300

    card3

--- src/app/card3/card3.component.ts
+++ src/app/card3/card3.component.ts
@@ -9,32 +9,44 @@ export class Card3Component implements OnInit {
 
   cards: any[] = [
     {
+      littleContainer: 'littleContainer1',
       header: 'Search Data',
       text: 'Dont worry if your data is very large, the Data Warehouse provides a search engine,' +
         ' wich is useful for making it easier to find data effectively saving time ',
       img: 'assets/layout/images/card31.png',
+      retangulo: 'assets/layout/images/retanguloCard31.png',
+      vector: 'vector',
+      elipse: 'elipse'
     }
-    // ,
-    // {
-    //   header: '24 Hours Access',
-    //   text: 'Access is given 24 hours a full morning to night and meet again in the morning, ' +
-    //     'giving you comfort when you need data when urgent',
-    //   img: 'assets/layout/images/card32.png',
-    //   background: 'rgba(246, 168, 248, 0.4)'
-    // },
-    // {
-    //   header: 'Print Out',
-    //   text: 'Print our service gives you vonvenience if someday you neeed print data, just edit it all and just print it.',
-    //   img: 'assets/layout/images/card33.png',
-    //   background: 'rgba(251, 215, 252, 0.4)'
-    // },
-    // {
-    //   header: 'Security Code',
-    //   text: 'Data Security is one of our best facilities. Allows for your files to be safer. The file can be secured' +
-    //     ' with a code or password tha you created, so only you can open the file.',
-    //   img: 'assets/layout/images/card34.png',
-    //   background: 'rgba(168, 248, 246, 0.4)'
-    // },
+    ,
+    {
+      littleContainer: 'littleContainer2',
+      header: '24 Hours Access',
+      text: 'Access is given 24 hours a full morning to night and meet again in the morning, ' +
+        'giving you comfort when you need data when urgent',
+      img: 'assets/layout/images/card32.png',
+      retangulo: 'assets/layout/images/retanguloCard32.png',
+      vector: 'vector2',
+    },
+    {
+      littleContainer: 'littleContainer3',
+      header: 'Print Out',
+      text: 'Print our service gives you vonvenience if someday you neeed print data, just edit it all and just print it.',
+      img: 'assets/layout/images/card33.png',
+      retangulo: 'assets/layout/images/retanguloCard33.png',
+      elipseG: 'elipseG3'
+
+    },
+    {
+      littleContainer: 'littleContainer4',
+      header: 'Security Code',
+      text: 'Data Security is one of our best facilities. Allows for your files to be safer. The file can be secured' +
+        ' with a code or password tha you created, so only you can open the file.',
+      img: 'assets/layout/images/card34.png',
+      retangulo: 'assets/layout/images/retanguloCard34.png',
+      vector: 'vector4',
+      elipseG: 'elipseG4'
+    },
   ];
 
   constructor() {
